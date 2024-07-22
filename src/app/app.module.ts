309eb5d8d3fb43c8193b7db02db10e5c13ddecc9
aniket3333 <anikethiware3@gmail.com>
2024-07-22 17:49:48 +0530

    added ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeLoginComponent } from './component/login/employee-login/employee-login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { EmployeeLoginComponent } from './component/login/employee-login/employe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
